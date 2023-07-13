b4c8e7904ca625bd01e236ed81ec830719ad3b7e
Kevin Dietmeyer <kevindietmeyer@Kevins-MacBook-Air-2.local>
2023-07-13 17:23:10 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+//import mongodb from 'mongodb'
+import 'dotenv/config'
+import mongodb , {MongoClient} from 'mongodb'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const client = new MongoClient(process.env.MONGO_URI)
+const db = client.db('my-form-database')
+
+const customers = db.collection('customers')
+
+app.post('/', async (req, res) => {
+  console.log('req from frontend --> ', req.body)
+
+  const newCustomer = await customers.insertOne(req.body)
+
+  res.status(200).send('Items Received')
+})
+
+app.listen(process.env.PORT, () => console.log('api listening on 4000 🐳 🦧'))
